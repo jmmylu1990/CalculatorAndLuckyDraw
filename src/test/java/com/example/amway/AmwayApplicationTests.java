1ea52667843c7ca3a8eed9685e75b494513060b7
vanliou <jmmylu1990@hotmail.com.tw>
2026-08-28 08:30:14 +0800

    chore: 初始化 Spring Boot 專案結構，整合 Web 與 Thymeleaf

---
+++ src/test/java/com/example/amway/AmwayApplicationTests.java
@@ -0,0 +1,1 @@
+package com.example.amway; import org.junit.jupiter.api.Test; import org.springframework.boot.test.context.SpringBootTest; @SpringBootTest class AmwayApplicationTests { @Test void contextLoads() {} }
