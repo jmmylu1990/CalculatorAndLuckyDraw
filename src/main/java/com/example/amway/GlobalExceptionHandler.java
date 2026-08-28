4f650f85373ad7135ba9b44ce9802207f8a060b0
vanliou <jmmylu1990@hotmail.com.tw>
2026-08-28 10:05:37 +0800

    docs: 新增專案 README 說明文件與優化全域例外處理器

---
+++ src/main/java/com/example/amway/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.amway;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public CalculatorResponse handleException(Exception e) {
+        return new CalculatorResponse("error", "系統錯誤: " + e.getMessage(), "0", java.util.List.of(), false, false, null);
+    }
+}
